1adc36265213da47135ea33a930cc569728114cf
guyk1978 <99318454+guyk1978@users.noreply.github.com>
2026-04-20 21:25:30 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.post("/scrape", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
